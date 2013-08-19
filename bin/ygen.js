722a5a1612e537615ce872ef0add9fc7567a53cc
Kevin Isom <kevin.isom@gmail.com>
2013-08-19 14:45:13 +1200

    working on model gen

--- bin/ygen.js
+++ bin/ygen.js
@@ -2,18 +2,33 @@
 
 var program = require('commander');
 
-var asciify = require('asciify'); 
-console.log(process.argv)
-    program
-        .version('0.0.1')
-        .option('-m, --model [name]', 'add model', 'model')
-        .option('-t, --template [name]', 'specify a template to be created', 'template.html')
-        .option('-v, --view', 'create a view')
-        .parse(process.argv);
+var asciify = require('asciify');  
+
+program
+    .version('0.0.1')
+    .option('-m, --model [name]', 'add model' )
+    .option('-t, --template [name]', 'specify a template to be created')
+    .option('-v, --view', 'create a view')
+    .option('-a, --attrs', 'Add attributes')
+    .parse(process.argv);
+
 
     
 asciify('YGen',  function (err, ascii) {
 
 	console.log(ascii.toString());
  	console.log(program.model);
+ 	if (program.model) {
+ 		generateModel({ model:program.model, attributes:program.attrs}, function (err, obj) {
+ 			if (err) {
+ 				console.log('bugger');
+ 				console.error(err);
+ 			} else {
+ 				console.log('The model was generated');
+ 			}
+ 		});
+ 	}
+
+
+ 	 
 });
