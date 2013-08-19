ae43ed500aa460d47eca902059128cc8773bd832
Kevin Isom <kevin.isom@gmail.com>
2013-08-20 07:45:22 +1200

    working on generate command

--- bin/ygen.js
+++ bin/ygen.js
@@ -6,19 +6,43 @@ var asciify = require('asciify');
 
 program
     .version('0.0.1')
-    .option('-m, --model [name]', 'add model' )
     .option('-t, --template [name]', 'specify a template to be created')
     .option('-v, --view', 'create a view')
     .option('-a, --attrs', 'Add attributes')
-    .parse(process.argv);
+    ;
+
+program
+    .command('generate [name]')
+    .description('generate objects')
+    .option('-m, --model [name] [fields]', 'add model' )
+    .action(function(name, options){
+        var t = [].slice.call(arguments, 0)
+        console.log('gen');
+        console.log(name);
+        //console.log(options)
+        console.log(arguments);
+        console.log(arguments.length);
+        for(var i = 0; i < arguments.length -1; i++) {
+            console.log(typeof arguments[i]);
+            if (typeof arguments[i] === 'string') {
+                console.log(arguments[i]); 
+            }
+        }
+    })
+
+
 
 
     
-asciify('YGen',  function (err, ascii) {
 
+var generateModel = require('../lib/models').modelGenerator;
+    
+asciify('YGen',  function (err, ascii) {
+    program.parse(process.argv);
 	console.log(ascii.toString());
- 	console.log(program.model);
+ 	//console.log(program);
  	if (program.model) {
+        console.log('gen model');
  		generateModel({ model:program.model, attributes:program.attrs}, function (err, obj) {
  			if (err) {
  				console.log('bugger');
